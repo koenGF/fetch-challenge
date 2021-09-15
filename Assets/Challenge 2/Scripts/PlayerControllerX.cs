eb805fa0eab0d1cd3da5770da42c2339f9c47989
Koen <2144264@talnet.nl>
2021-09-15 10:07:00 +0200

    Initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    float waitTime = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // subtract waited time from wait time
+        waitTime -=Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+           // allow spawn after wait time is over
+            if(waitTime <= 0)
+            {
+                SpawnDog();
+
+                //reset wait time
+                waitTime = 1;
+            } 
+        }
+    }
+
+void SpawnDog()
+    {
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+    }
+
+}
